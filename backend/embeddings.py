702b6699b3c41bfde6345bdf48b0a78af56c8cbc
Rudy <rudraksh.monga@mail.utoronto.ca>
2024-09-28 16:21:21 -0400

    updated backend stuff

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -1,6 +1,9 @@
 import os
 import cohere
+import vecs
 from config import settings
+
+vx = vecs.create_client(settings.DB_CONNECTION_STRING)
 
 
 def load_captions():
@@ -11,9 +14,29 @@ def load_captions():
 
 
 def save_embeddings(batch, embeddings):
-    for caption, embed in zip(batch, embeddings):
-        # update databser for caption['id'] with embed
-        pass
+    docs = vx.get_or_create_collection(name="image_embeddings", dimension=2)
+    # docs contains :
+    # vector[0]: uuid -> same as image uuid
+    # vector[1]: embedding -> the float embeddings
+
+    # NOTE: pls update the uuid field to match what batch contains
+
+    docs.upsert(
+        records=[
+            (uuid, embedding) for uuid, embedding in zip(batch, embeddings)
+        ]
+    )
+
+    pass
+    
+    # for caption, embed in zip(batch, embeddings):
+    #     # update databser for caption['id'] with embed
+    #     docs.upsert(
+    #         records = [
+
+    #         ]
+    #     )
+    #     pass
 
 
 def main():
@@ -38,6 +61,14 @@ def main():
         )
 
         save_embeddings(batch, response.embeddings.float)
+    
+    docs = vx.get_or_create_collection(name="image_embeddings", dimension=2)
+    docs.create_index(
+        method=IndexMethod.hnsw,
+        measure=IndexMeasure.cosine_distance,
+    )
+
+    
 
 
 if __name__ == "__main__":
