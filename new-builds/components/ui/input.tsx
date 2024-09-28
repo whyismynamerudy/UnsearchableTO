9dde2763e4b7a8fd0effdea5a1627da80e07b516
Arsham Ghahramani <arsham@ribbon.cool>
2024-09-28 18:40:37 -0400

    disable

--- new-builds/components/ui/input.tsx
+++ new-builds/components/ui/input.tsx
@@ -1,8 +1,8 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-export interface InputProps
+export interface InputProps // eslint-disable-line
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
@@ -17,9 +17,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
