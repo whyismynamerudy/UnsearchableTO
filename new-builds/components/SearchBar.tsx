8c1aa89d583d1428bf5da12372f33b93a423fb7f
rik <vslimrik@gmail.com>
2024-09-29 14:22:43 -0400

    update frontend

--- new-builds/components/SearchBar.tsx
+++ new-builds/components/SearchBar.tsx
@@ -29,41 +29,41 @@ export default function SearchBar({ onSearch }: SearchBarProps) {
   };
 
   return (
-    <div>
+    <div className='absolute'>
       <form
         onSubmit={handleSearch}
         className='relative w-full max-w-2xl mx-auto'
       >
         <div className='flex items-center space-x-2'>
           <div className='relative flex-grow'>
-            <Search className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400' />
+            <Search className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500' />
             <Input
               type='text'
               placeholder='Search locations...'
               value={query}
               onChange={(e) => setQuery(e.target.value)}
-              className='pl-10 pr-4 py-3 w-full text-sm md:text-base bg-[#2d3748] text-white border-gray-400 rounded-lg'
+              className='pl-10 pr-4 py-3 w-full text-sm md:text-base bg-white text-gray-800 border-gray-300 rounded-lg'
               style={{ fontSize: '16px' }}
             />
           </div>
 
           <Button
             type='submit'
-            className='bg-[#f7fafc] hover:bg-[#e2e8f0] text-[#1a202c] font-semibold py-3 px-6 rounded-lg transition-all duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-[#38a89d] focus:ring-opacity-50'
+            className='bg-[#4fd1c5] hover:bg-[#48a198] text-gray-700 font-semibold py-3 px-6 rounded-lg transition-all duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-[#38a89d] focus:ring-opacity-50'
           >
             Search
           </Button>
         </div>
       </form>
 
-      {/* Sample Prompts as Buttons */}
-      <div className='flex flex-wrap mt-4 justify-center space-x-2'>
+      {/* Sample Prompts as Buttons in One Line */}
+      <div className='flex flex-row mt-4 justify-center space-x-2'>
         {samplePrompts.map((prompt, index) => (
           <Button
             key={index}
             type='button'
             onClick={() => handleSelect(prompt)}
-            className='bg-[#e2e8f0] hover:bg-[#cbd5e0] text-[#1a202c] font-semibold py-2 px-4 rounded-lg transition-all duration-300 transform hover:scale-105 focus:outline-none'
+            className='w-full bg-[#f7fafc] hover:bg-[#e2e8f0] text-gray-800 font-semibold py-3 px-6 rounded-lg transition-all duration-300 transform hover:scale-105 focus:outline-none focus:bg-[#4fd1c5] focus:ring-2 focus:ring-[#38a89d] focus:ring-opacity-50'
           >
             {prompt}
           </Button>
