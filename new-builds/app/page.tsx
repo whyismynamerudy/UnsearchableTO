569cb21b0edf21bdbfac7bfd92c22ab7456e8fa4
rik <vslimrik@gmail.com>
2024-09-28 23:04:43 -0400

    fix

--- new-builds/app/page.tsx
+++ new-builds/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
       }
       const results = await response.json();
 
-      setMarkers(results);
+      setMarkers(results.results);
       setError(null);
     } catch (error) {
       console.error('Error fetching markers:', error);
