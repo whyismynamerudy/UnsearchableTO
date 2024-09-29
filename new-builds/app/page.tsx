230f3d7a362520812abac89de40877c42249a225
Arsham Ghahramani <arsham@ribbon.cool>
2024-09-29 15:04:51 -0400

    update copy

--- new-builds/app/page.tsx
+++ new-builds/app/page.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import Map from '@/components/Map';
-import SearchBar from '@/components/SearchBar';
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-import { AlertCircle } from 'lucide-react';
-import Image from 'next/image';
-import { useState } from 'react';
-import torontoLogo from './icons/toronto_logo.png';
+import Map from "@/components/Map";
+import SearchBar from "@/components/SearchBar";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircle } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+import torontoLogo from "./icons/toronto_logo.png";
 
 export interface MarkerDetails {
   image_id: string;
@@ -35,15 +35,15 @@ export default function Home() {
   const [showMarkers, setShowMarkers] = useState(true);
   const [showHeatmap, setShowHeatmap] = useState(false);
 
-  const handleToggle = (type: 'markers' | 'heatmap') => {
-    if (type === 'markers') {
+  const handleToggle = (type: "markers" | "heatmap") => {
+    if (type === "markers") {
       setShowMarkers(!showMarkers);
-    } else if (type === 'heatmap') {
+    } else if (type === "heatmap") {
       setShowHeatmap(!showHeatmap);
     }
   };
   const handleSearch = async (query: string) => {
-    console.log('Search query:', query);
+    console.log("Search query:", query);
 
     if (!query) {
       setMarkers([]);
@@ -61,15 +61,15 @@ export default function Home() {
         )}`
       );
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       const results = await response.json();
 
       setMarkers(results.results.slice(0, 5));
       setHeatmapData(results.heatmap_data.slice(0, 5));
     } catch (error) {
-      console.error('Error fetching markers:', error);
-      setError('Failed to fetch markers. Please try again.');
+      console.error("Error fetching markers:", error);
+      setError("Failed to fetch markers. Please try again.");
       setMarkers([]);
     } finally {
       setIsLoading(false);
@@ -77,27 +77,27 @@ export default function Home() {
   };
 
   return (
-    <div className='flex flex-col h-screen'>
-      <header className='bg-white shadow-lg z-20'>
-        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 flex items-center justify-between'>
-          <div className='flex flex-row items-center'>
+    <div className="flex flex-col h-screen">
+      <header className="bg-white shadow-lg z-20">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 flex items-center justify-between">
+          <div className="flex flex-row items-center">
             <Image
               src={torontoLogo}
-              alt='Toronto Logo'
+              alt="Toronto Logo"
               width={40} // Adjust width as needed
               height={40} // Adjust height as needed
-              className='mr-2' // Margin to separate the logo from the text
+              className="mr-2" // Margin to separate the logo from the text
             />
-            <h1 className='text-3xl font-bold text-black'>TorontoVision</h1>
+            <h1 className="text-3xl font-bold text-black">Unsearchable TO</h1>
           </div>
 
-          <div className='text-xl font-bold italic text-[#2d3748]'>
+          <div className="text-xl font-bold italic text-[#2d3748]">
             511k+ views indexed
           </div>
         </div>
       </header>
-      <div className='relative flex-grow overflow-hidden'>
-        <div className='absolute inset-0 h-full'>
+      <div className="relative flex-grow overflow-hidden">
+        <div className="absolute inset-0 h-full">
           <Map
             markers={markers}
             heatmapData={heatmapData}
@@ -108,50 +108,50 @@ export default function Home() {
 
         {/* Loading Spinner */}
         {isLoading && (
-          <div className='absolute inset-0 flex justify-center items-center z-10'>
-            <div className='animate-spin rounded-full h-16 w-16 border-t-4 border-[#4fd1c5]'></div>
+          <div className="absolute inset-0 flex justify-center items-center z-10">
+            <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-[#4fd1c5]"></div>
           </div>
         )}
 
         {/* Overlay content */}
-        <div className='pb-4 absolute top-10 left-10'>
-          <div className='flex space-x-4'>
+        <div className="pb-4 absolute top-10 left-10">
+          <div className="flex space-x-4">
             <div
-              onClick={() => handleToggle('markers')}
+              onClick={() => handleToggle("markers")}
               className={`${
                 showMarkers
-                  ? 'bg-[#4fd1c5] text-black border-transparent shadow-lg' // Selected state
-                  : 'bg-white text-black border-gray-300 hover:bg-gray-100' // Unselected state
+                  ? "bg-[#4fd1c5] text-black border-transparent shadow-lg" // Selected state
+                  : "bg-white text-black border-gray-300 hover:bg-gray-100" // Unselected state
               } px-6 py-3 text-sm font-semibold rounded-lg border transition-all duration-200 cursor-pointer`}
             >
               Markers
             </div>
             <div
-              onClick={() => handleToggle('heatmap')}
+              onClick={() => handleToggle("heatmap")}
               className={`${
                 showHeatmap
-                  ? 'bg-[#4fd1c5] text-black border-transparent shadow-lg' // Selected state
-                  : 'bg-white text-black border-gray-300 hover:bg-gray-100' // Unselected state
+                  ? "bg-[#4fd1c5] text-black border-transparent shadow-lg" // Selected state
+                  : "bg-white text-black border-gray-300 hover:bg-gray-100" // Unselected state
               } px-6 py-3 text-sm font-semibold rounded-lg border transition-all duration-200 cursor-pointer`}
             >
               Heatmap
             </div>
           </div>
         </div>
-        <main className='absolute top-10 left-1/2 transform -translate-x-1/2 w-full max-w-md px-4'>
-          <div className='space-y-8 flex flex-col items-center'>
+        <main className="absolute top-10 left-1/2 transform -translate-x-1/2 w-full max-w-md px-4">
+          <div className="space-y-8 flex flex-col items-center">
             <SearchBar onSearch={handleSearch} />
 
             {error && (
               <Alert
-                variant='destructive'
-                className='bg-red-900/50 border-red-700 w-full'
+                variant="destructive"
+                className="bg-red-900/50 border-red-700 w-full"
               >
-                <AlertCircle className='h-5 w-5 text-red-400' />
-                <AlertTitle className='text-red-400 font-semibold'>
+                <AlertCircle className="h-5 w-5 text-red-400" />
+                <AlertTitle className="text-red-400 font-semibold">
                   Error
                 </AlertTitle>
-                <AlertDescription className='text-red-200'>
+                <AlertDescription className="text-red-200">
                   {error}
                 </AlertDescription>
               </Alert>
